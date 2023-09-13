77c6e48486e4235d60abc0120e5bf619cc536a1a
Tomasz Roszczynialski <roszcz@gmail.com>
2023-09-13 17:59:06 +0200

    more leet css hacking

--- scripts/algrtm_tags.js
+++ scripts/algrtm_tags.js
@@ -13,3 +13,10 @@ hexo.extend.tag.register('algrtmAudio', function(args) {
     Your browser does not support the audio element.
   </audio>`;
 }, {ends: false});
+
+hexo.extend.tag.register('algrtmImgBanner', function(args) {
+  // TODO: Would be great to get this with a hexo function :thinking:
+  var src = '/blog/assets/' + args[0];
+  var alt = args[1] || '';
+  return `<img src="${src}" alt="${alt}" style="width: calc(100% + 20px); display: block; overflow: visible; float: center; margin-left: -10px; margin-right: -10px"><div style="clear: both;"></div>`;
+}, {ends: false});
